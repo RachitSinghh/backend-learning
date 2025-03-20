fa9142582a69dbc5f824c7637d743df605bf3d44
RachitSinghh <singhrachit@outlook.in>
2025-03-21 02:42:42 +0530

    completed online courses platform schema making

--- models/Online Course Platform/submission.models.js
+++ models/Online Course Platform/submission.models.js
@@ -1,6 +1,26 @@
 import mongoose from "mongoose";
 
-const submissionSchema = new mongoose.Schema({}, {timestamps:true})
+const submissionSchema = new mongoose.Schema({
+  assignmentId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: "Assignment",
+    required: true, 
+  },
+  studentId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: true, 
+  },
+  submittedAt:{
+    type: Date,
+    default: Date.now
+  },
+  score:{
+    type: Number,
+    required: true
+  }
+}, {timestamps:true})
 
 export const Submission = mongoose.model("Submission", submissionSchema); 
 
+//Submission: assignmentId, studentId, submittedAt, score
